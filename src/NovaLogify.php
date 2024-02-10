12e10e731207ad1a66cf4ce6791be3cd0c84d798
GigaGiorgadze <giga.giorgadze.11@gmail.com>
2024-02-10 13:55:40 +0400

    initial commit

---
+++ src/NovaLogify.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SoMany\NovaLogify;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Menu\MenuSection;
+use Laravel\Nova\Nova;
+use Laravel\Nova\Tool;
+
+class NovaLogify extends Tool
+{
+    /**
+     * Perform any tasks that need to happen when the tool is booted.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Nova::script('nova-logify', __DIR__ . '/../dist/js/tool.js');
+        Nova::style('nova-logify', __DIR__ . '/../dist/css/tool.css');
+    }
+
+    /**
+     * Build the menu that renders the navigation links for the tool.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return mixed
+     */
+    public function menu(Request $request)
+    {
+        return MenuSection::make('Nova Logify')
+            ->path('/nova-logify')
+            ->icon('server');
+    }
+}
